040d8c69196dd1bd4be18da14eaad2cbb0792362
Nikodem <wronanikodem@gmail.com>
2021-12-21 15:02:05 +0100

    feat: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 
 # Customize $PATH
-set -x PATH $PATH /sbin/:/opt/homebrew/bin:/Users/nikodem/.cargo/bin
+set -x PATH $PATH /sbin/:/opt/homebrew/bin:$HOME/.cargo/bin:$HOME/.local/go/bin
 
 # Aliases
 alias src="source ~/.config/fish/config.fish"
@@ -12,7 +12,8 @@ alias mv="mv -vi"
 alias rm="rm -i"
 alias vim="nvim"
 alias vi="nvim"
-alias ls="colorls"
+alias ll="colorls -l"
+alias ls="colorls -l"
 alias l="colorls"
 alias py="python3"
 
@@ -24,9 +25,5 @@ fnm env | source
 # Init starship for fish shell
 starship init fish | source
 
-# Init Anaconda
-eval /Users/nikodem/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-
 # Run custom scripts
 ssh-add ~/.ssh/github/github
-
